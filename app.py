22710341a3af2dcbfef8e05d62dc7e746ae32571
pedrowebster <94661014+pedrowebster@users.noreply.github.com>
2024-12-02 20:20:18 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -24,12 +24,13 @@ st.markdown(texto)
 
 
 ## Visualizacao no streamlit
-aba1, aba2 = st.tabs(['Dashboard & Insights', 'Modelo Preditivo'])
+
+aba1, aba2, aba3 = st.tabs(['Dashboard & Insights', 'Modelo Preditivo', 'Código do Modelo'])
 
 with aba2:
     st.subheader('Previsão do Preço de Petróleo (US$) Utilizando a Biblioteca Prophet')
 
-    st.caption('''Este projeto utiliza a biblioteca Prophet para prever o preço diário em US$ do barril de Petróleo. O modelo
+    st.write('''Este projeto utiliza a biblioteca Prophet para prever o preço diário em US$ do barril de Petróleo. O modelo
             criado foi treinado com dados do ano de 2024 (até o dia 9 de Setembro) e possui as seguintes métricas de acurária (MSE: 11.92, RMSE: 3.45 e MAPE: 2.82 %).
             O usuário pode inserir o número de dias para os quais deseja a previsão, e o modelo gerará um gráfico
             interativo contendo as estimativas baseadas em dados históricos de preço.
@@ -77,9 +78,87 @@ with aba1:
     
     st.subheader('Dashboard Looker com Insights Sobre Variação do Preço do Petróleo')
 
-    st.caption('''O dashboard abaixo foi feito utilizando a ferramenta de BI Looker, da Google. Todos os insights a respeito da variação do preço do Petróleo relacionando com os 
-               acontececimentos relavantes estão expressos dentro dele (na parte inferior).''')
+    st.write('''Para atender ao pedido do cliente, optamos por desenvolver o dashboard no Looker Studio, uma ferramenta online que transforma dados em dashboards interativos e personalizáveis.
+               
+O primeiro elemento incluído no dashboard foram as métricas estatísticas, que oferecem uma visão geral do comportamento dos preços do petróleo, destacando o preço máximo, mínimo e médio históricos.
+               
+Em seguida, adicionamos gráficos sob duas perspectivas:
+               
+- A primeira (ver primeiro gráfico de linhas) mostra um registro diário dos preços do petróleo Brent, permitindo a visualização das variações ao longo do tempo, com dados exclusivamente na granularidade diária.
+               
+- A segunda (ver segundo gráfico de linhas) também exibe registros diários dos preços do Brent, mas segmenta-os por eventos históricos relevantes, como a pandemia de COVID-19. Neste gráfico, habilitamos a funcionalidade "Detalhar", que possibilita ao usuário explorar dados em diferentes níveis de granularidade ao clicar na seta acima do gráfico.
+               
+Por fim, adicionamos filtros que permitem aos usuários pesquisar não apenas por períodos específicos, mas também por eventos históricos que impactaram significativamente os preços do petróleo.
+
+Em resumo, nosso objetivo foi desenvolver um dashboard em uma plataforma intuitiva, proporcionando aos clientes uma experiência simples e direta, com opções de filtros úteis e um design visualmente agradável.
+''')
     
     # The Google Looker Studio embed URL
     looker_studio_url = "https://lookerstudio.google.com/embed/reporting/f28ef11e-27fe-4ceb-b1d1-5fc5060a4db8/page/0yVWE"
     components.iframe(looker_studio_url, width=1000, height=1280)
+
+with aba3:
+
+    st.write('''Abaixo segue o código em python da construção do modelo utilizando a biblioteca Prophet. Para ter acesso ao código na integra, acessar o jupyter notebook (TechChallenge_prophet_v2.ipynb) disponibilizado.''')
+
+    code = '''
+    import pandas as pd
+    import plotly.express as px
+    import numpy as np
+    from prophet import Prophet
+    import matplotlib.pyplot as plt
+    from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+    url = "https://raw.githubusercontent.com/pedrowebster/Prophet_alura/refs/heads/main/df.csv"
+
+    df = pd.read_csv(url)
+    df = df.drop(df.columns[0], axis=1)
+    df.columns = ["Data", "Preco"]
+    df["Preco"] = pd.to_numeric(df["Preco"], errors="coerce").astype("Int64")
+    df["Preco"] = df["Preco"]/100
+    df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y", errors="coerce")
+    df = df.sort_index(ascending=False).reset_index()
+    df.drop(df.columns[0], axis = 1, inplace=True)
+
+    df_prophet = pd.DataFrame()
+    df_prophet['ds'] = df['Data']
+    df_prophet['y'] = df['Preco']
+
+    data_de_corte = df_prophet['ds'].max()
+    data_de_corte = data_de_corte - pd.DateOffset(years=1)
+
+    df_prophet_2 = df_prophet[df_prophet['ds'] >= data_de_corte]
+    df_prophet_2.reset_index(inplace=True)
+    fig = px.line(df_prophet_2, x='ds', y='y')
+
+    tamanho_treino = int(len(df_prophet_2)* 0.8)
+    tamanho_teste = int(len(df_prophet_2) * 0.2)
+
+    df_treino = pd.DataFrame()
+    df_treino['ds'] = df_prophet_2['ds'][:tamanho_treino]
+    df_treino['y'] = df_prophet_2['y'][:tamanho_treino]
+
+    df_teste = pd.DataFrame()
+    df_teste['ds'] = df_prophet_2['ds'][tamanho_treino:]
+    df_teste['y'] = df_prophet_2['y'][tamanho_treino:]
+
+    # CRIANDO O MODELO
+    # Definir um seed
+    np.random.seed(4587)
+
+    # Instanciar o modelo Prophet
+    modelo = Prophet(yearly_seasonality=True)
+
+    # Treinar o modelo
+    modelo.fit(df_treino)
+
+    # Criar um dataframe para previsões futuras
+    futuro = modelo.make_future_dataframe(periods = tamanho_teste, freq='B')
+    previsao = modelo.predict(futuro)
+
+    # AVALIANDO MÉTRICAS DE ACURÁRIA
+    mse = mean_squared_error(df_comparacao['y'], df_comparacao['yhat']).round(2)
+    rmse = np.sqrt(mse).round(2)
+    mape = mean_absolute_error(df_comparacao['y'], df_comparacao['yhat']).round(2)'''
+    st.code(code, language="python")
+    st.caption('''Também foi construído um modelo secundário, presente no jupyter notebook TechChallengeXGBoost.ipynb, no entanto, este não foi escolhido pois apresentou métricas de acurácia menores.''')
